2e8faa3539c8a57a610f0708cd94f8475d742dda
HyeonJun <widrn1010@live.co.kr>
2023-06-20 18:01:27 +0900

    edu02

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,27 @@
 <template>
-	<div class="name">{{ name }}</div>
-	<button class="btn btn-primary" v-on:click="updateName">click</button>
+	<div class="container">
+		<h1>ToDo List</h1>
+		<form action="#" class=" mb-4" @submit.prevent="onSubmit">
+			<div class="d-flex">
+				<div class="flex-grow-1 mr-2">
+					<input id="name" v-model="todo" class="form-control" name="name" placeholder="Type new To-Do" type="text">
+				</div>
+				<div>
+					<button class="btn btn-primary">Add</button>
+				</div>
+			</div>
+			<div v-show="hasError" style="color: red">This field cannt not</div>
+		</form>
+		<div v-for="todo in todos" :key="todo.id" class="card mb-2">
+			<div class="card-body p-2 d-flex justify-content-between align-items-center">
+				<div class="form-check">
+					<input :id="'todo_'+todo.id" v-model="todo.complated" :name="'todo_'+todo.id" class="form-check-input" type="checkbox" />
+					<label :for="'todo_'+todo.id" :style="todo.complated ? todoSyle : {}" class="form-check-label">{{ todo.subject }}</label>
+				</div>
+				<button class="btn btn-danger btn-sm" type="button">Delete</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -8,27 +29,53 @@ import { ref } from "vue";
 
 export default {
 	setup() {
-		let name = ref({id: 1});
+		let todo = ref("");
+		const todos = ref([{
+			id       : 1,
+			subject  : "휴대폰 사기",
+			complated: false,
+		}, {
+			id       : 2,
+			subject  : "장보기",
+			complated: false,
+		}]);
 
-		const updateName = () => {
-			name.value.id = 2;
+		const hasError = ref(false);
+
+		const todoSyle = {
+			textDecoration: "line-through",
+			color         : "lightgray",
 		};
 
-		/*const greeting = (name) => {
-			return "hello " + name;
+		const onSubmit = () => {
+			if (todo.value !== "") {
+				todos.value.push({
+					id     : Date.now(),
+					subject: todo.value,
+				});
+
+				todo.value = "";
+				hasError.value = false;
+			} else {
+				hasError.value = true;
+			}
 		};
-		*/
+
 		return {
-			name,
-			updateName,
-			// greeting,
+			todo,
+			todos,
+			onSubmit,
+			hasError,
+			todoSyle,
 		};
 	},
 };
 </script>
 
 <style>
-.name {
-	color: deepskyblue;
+label,
+input[type=radio],
+input[type=checkbox] {
+	cursor: pointer;
 }
 </style>
