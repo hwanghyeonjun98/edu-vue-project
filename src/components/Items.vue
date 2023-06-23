09b6993eecf0c7dc2878ad44794257e0cdc52e41
HyeonJun <widrn1010@live.co.kr>
2023-06-23 14:53:12 +0900

    edu08

---
+++ src/components/Items.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="card mb-2" v-for="(item, idx) in items" :key="item.id">
+		<slot :idx="idx" :item="item"></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		items: {
+			type   : Array,
+			require: true,
+		},
+	},
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/components/TodoItems.vue
+++ src/components/TodoItems.vue
@@ -1,15 +1,17 @@
 <template>
-	<div class="card mb-2" v-for="(todo, idx) in todos" :key="todo.id">
-		<div class="card-body p-2 d-flex justify-content-between align-items-center" @click="moveToPage(todo.id)">
-			<div class="form-check">
-				<input type="checkbox" class="form-check-input" :id="'todo_'+todo.id" :checked="todo.completed" :name="'todo_'+todo.id" @change="toggleTodo(idx, $event)" @click.stop />
-				<!-- <label :for="'todo_'+todo.id" :style="todo.completed ? todoSyle : {}" class="form-check-label">{{ todo.subject }}</label> -->
-				<label class="form-check-label" :class="{todo: todo.completed}" :for="'todo_'+todo.id" @click.stop>{{ todo.subject }}</label>
-			</div>
+	<Items :items="todos">
+		<template #default="{item, idx}">
+			<div class="card-body p-2 d-flex justify-content-between align-items-center" @click="moveToPage(item.id)">
+				<div class="form-check">
+					<input type="checkbox" class="form-check-input" :id="'todo_'+ item.id" :checked="item.completed" :name="'todo_'+item.id" @change="toggleTodo(idx, $event)" @click.stop />
+					<!-- <label :for="'todo_'+todo.id" :style="todo.completed ? todoSyle : {}" class="form-check-label">{{ todo.subject }}</label> -->
+					<label class="form-check-label" :class="{todo: item.completed}" :for="'todo_'+item.id" @click.stop>{{ item.subject }}</label>
+				</div>
 
-			<button type="button" class="btn btn-danger btn-sm" @click.stop="openModal(todo.id)">Delete</button>
-		</div>
-	</div>
+				<button type="button" class="btn btn-danger btn-sm" @click.stop="openModal(todo.id)">Delete</button>
+			</div>
+		</template>
+	</Items>
 	<teleport to="#modal">
 		<Modal v-if="isShowModal" @close="closeModal" @delete="deleteTodo">
 		</Modal>
@@ -20,10 +22,12 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import Modal from "@/components/DeleteModal.vue";
+import Items from "@/components/Items.vue";
 
 export default {
 	components: {
 		Modal,
+		Items,
 	},
 	props     : {
 		todos: {
